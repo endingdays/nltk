6c7d88a8a3f2de2712ec2062aaeadbefb3452b3b
Paul Mospan <pavel.mospan@yahoo.com>
2019-08-14 00:46:58 +0300

    downloaded section books

---
+++ main.py
@@ -0,0 +1,11 @@
+import nltk
+
+# browse the available packages using nltk.download().
+# install the data required and select the book collection.
+# BOOK consists of about 30 compressed files requiring about 100Mb disk space. 
+# The full collection of data (i.e., all in the downloader) is nearly ten times this size
+nltk.download()
+#  loading some texts for us to explore: 
+from nltk.book import *
+
+text1
