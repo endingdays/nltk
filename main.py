a514900d8c79b2199cece56cf518a38f21f13646
Paul Mospan <pavel.mospan@yahoo.com>
2019-08-15 15:49:43 +0300

    added more precise descr and switched to pycharm from MVS

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-import nltk
-
-# browse the available packages using nltk.download() 
-# install the data required and select the book collection.
-# BOOK consists of about 30 compressed files requiring about 100Mb disk space. 
+# Before going further you should install NLTK 3.0:
+#       pip install nltk
+# Then install the data required for the book by typing the following two commands,
+# then selecting the book collection from an available packages :
+#       import nltk
+#       nltk.download()
+# to install the data required
+#  BOOK consists of about 30 compressed files requiring about 100Mb disk space.
 # The full collection of data (i.e., all in the downloader) is nearly ten times this size
-# I have alredy installed books on my machine, so commented out the above displayed command
+# Once you run installed nltk and executed 2 commands above you are free to use collection of books, provided
+# by nltk anywhere and whenever. Also typing
+#       nltk.download()
+# browse more data given by nltk creators
 
 #  loading some texts for us to explore: 
 from nltk.book import *
-
 
 print('this is text1: ', text1)
