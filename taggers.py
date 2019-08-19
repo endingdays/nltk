f8f0354f6e1d1745269553a7020da96e44820203
Paul Mospan <pavel.mospan@yahoo.com>
2019-08-19 18:30:33 +0300

    finding similar context to the words

--- taggers.py
+++ taggers.py
@@ -15,3 +15,10 @@ nltk.help.upenn_tagset('DT')
 nltk.help.upenn_tagset('VBP')
 nltk.help.upenn_tagset('PRP')
 nltk.help.upenn_tagset('NNS')
+
+# .similar() method takes a word w, finds all contexts w1w w2, then finds all words w' that appear in the same context
+text = nltk.Text(word.lower() for word in nltk.corpus.brown.words())
+print("text.similar('woman'): ")
+text.similar('woman')
+print("text.similar('buy'): ")
+text.similar('buy')
